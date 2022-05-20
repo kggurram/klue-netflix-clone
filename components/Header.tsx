e3e3e7647fc2c89abe4d44601bae4c5f3cb664ac
Karthik Gurram <kggurram@gmail.com>
2022-05-20 01:10:14 -0400

    home ui complete

---
+++ components/Header.tsx
@@ -0,0 +1,59 @@
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className="md: flex items-center space-x-2 md:space-x-10">
+        <img
+          src="logo.png"
+          width={50}
+          height={50}
+          className="cursor-pointer rounded-xl object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://avatars.githubusercontent.com/u/40126108?s=400&u=a9bf983b36a4c5b4efe3d933b646a5c46342de42&v=4"
+            alt=""
+            className="h-10 w-10 cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
